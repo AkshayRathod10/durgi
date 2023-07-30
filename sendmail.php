208da7aae0adb277e87bf735ae15d85233669b4e
Akshay Rathod <akshayrathod9@gmail.com>
2023-07-30 12:38:27 +0530

    social media links

--- sendmail.php
+++ sendmail.php
@@ -21,27 +21,34 @@ $education = isPost('education');
 $requirement = isPost('requirement');
 
 echo "<div>Name: $name </div><div>Number: $number </div><div>Email: $email</div><div>Education: $education</div><div>Requirement: $requirement</div>";
-die();
 
 
-$mail = PHPMailer();
 
-$mail->SMTPDebug = 2;                   // Enable verbose debug output
-$mail->isSMTP();                        // Set mailer to use SMTP
-$mail->Host       = 'smtp.gfg.com;';    // Specify main SMTP server
-$mail->SMTPAuth   = true;               // Enable SMTP authentication
-$mail->Username   = 'user@gfg.com';     // SMTP username
-$mail->Password   = 'password';         // SMTP password
-$mail->SMTPSecure = 'tls';              // Enable TLS encryption, 'ssl' also accepted
-$mail->Port       = 587; 
+try{
 
-$mail->setFrom('from@gfg.com', 'Name');           // Set sender of the mail
-$mail->addAddress('receiver1@gfg.net');           // Add a recipient
-$mail->addAddress('receiver2@gfg.com', 'Name');   // Name is optional
+    $mail = new PHPMailer(true);
 
-$mail->isHTML(true);                                  
-$mail->Subject = 'Durgi Immigration Specialist - Inquire';
-$mail->Body    = "<div>Name: $name </div><div>Number: $number </div><div>Email: $email</div><div>Education: $education</div><div>Requirement: $requirement</div>";
-$mail->AltBody = 'Body in plain text for non-HTML mail clients';
-
-$mail->send();
+    $mail->SMTPDebug = 1;                   // Enable verbose debug output
+    $mail->isSMTP();                        // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';    // Specify main SMTP server
+    $mail->SMTPAuth   = true;               // Enable SMTP authentication
+    $mail->Username   = 'durgiimmigrationspecialist@gmail.com';     // SMTP username
+    $mail->Password   = 'Durgi@2023';         // SMTP password
+    $mail->SMTPSecure = 'tls';              // Enable TLS encryption, 'ssl' also accepted
+    $mail->Port       = 587; 
+    
+    $mail->setFrom('durgiimmigrationspecialist@gmail.com', 'Durgi Immigration Specialist');           // Set sender of the mail
+    $mail->addAddress('akshayrathod9@gmail.com');           // Add a recipient
+    
+    $mail->isHTML(true);                                  
+    $mail->Subject = 'Durgi Immigration Specialist - Inquire';
+    $mail->Body    = "<div>Name: $name </div><div>Number: $number </div><div>Email: $email</div><div>Education: $education</div><div>Requirement: $requirement</div>";
+    $mail->AltBody = 'Body in plain text for non-HTML mail clients';
+    
+    $mail->send();
+}
+catch (phpmailerException $e) {
+    echo $e->errorMessage(); //Pretty error messages from PHPMailer
+  } catch (Exception $e) {
+    echo $e->getMessage(); //Boring error messages from anything else!
+  }
